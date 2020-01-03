0361094ab71f1b4f6d7484100c04faec71b56b7d
Rubaiyat Jahan Mumu <rubaiyat.jahan@gmail.com>
2020-01-03 11:05:40 -0500

    Update CountAndSay.java

--- LeetCode/CountAndSay.java
+++ LeetCode/CountAndSay.java
@@ -5,31 +5,19 @@ class Solution {
         if(n==1){
             return "1";
         }
-        
         for(int i=1;i<n;i++){
-            HashMap<Character,Integer> map = new HashMap<Character,Integer>();
+            int count = 1;
             int size = answer.length();
             StringBuilder sb = new StringBuilder();
-            for(int j=0;j<size;j++){
-                //char c = 
-                if(map.containsKey(answer.charAt(j))){
-                    int count = map.get(answer.charAt(j));
-                    map.put(answer.charAt(j), count + 1);
-                }else {
-                    if(!map.isEmpty()) {
-                        sb.append(map.get(answer.charAt(j-1)));
-                        sb.append(answer.charAt(j-1));
-                        map.clear();
-                    }
-                    map.put(answer.charAt(j),1);
+            int j;
+            for(j=1;j<size;j++){ 
+                if(answer.charAt(j) == answer.charAt(j-1)) count++;
+                else {
+                    sb.append(count + "" + answer.charAt(j-1));
+                    count = 1;
                 }
             }
-            if(!map.isEmpty()) {
-                sb.append(map.get(answer.charAt(size-1)));
-                sb.append(answer.charAt(size-1));
-                //map.clear();
-            }
-            
+            if(j == size) sb.append(count + "" + answer.charAt(j-1));
             answer = sb.toString();
         }
         return answer;
