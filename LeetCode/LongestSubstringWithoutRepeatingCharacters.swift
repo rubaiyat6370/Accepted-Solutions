c3b9334bbf8cd9dad9b025f05650283ba53f815b
Rubaiyat Jahan Mumu <rubaiyat.jahan@gmail.com>
2020-03-12 12:23:09 -0400

    Create LongestSubstringWithoutRepeatingCharacters.swift

---
+++ LeetCode/LongestSubstringWithoutRepeatingCharacters.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func lengthOfLongestSubstring(_ s: String) -> Int {
+        var set = Set<Character>()
+        var start = 0, end = 0, maxLength = 0
+        let arr = Array(s)
+        while end < arr.count {
+            if !set.contains(arr[end]) {
+                set.insert(arr[end])
+                end += 1
+            } else {
+                set.remove(arr[start])
+                //set.insert(arr[end])
+                start += 1
+                //end += 1
+            }
+            maxLength = max(maxLength, end-start)
+        }
+        return maxLength
+    }
+}
